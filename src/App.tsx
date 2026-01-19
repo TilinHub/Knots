be196a82d942d7bc900b1ce8fc165e0e873ccdf7
TilinHub <tomasayalacoppola07@gmail.com>
2026-01-19 16:30:55 -0300

    Update App.tsx to show KnotGallery as initial view

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { useState } from 'react';
 import { EditorPage } from "./features/editor/EditorPage";
+import { KnotGallery } from "./features/gallery/KnotGallery";
 
 export default function App() {
+  const [showGallery, setShowGallery] = useState(true);
+
+  if (showGallery) {
+    return <KnotGallery onSelectKnot={() => setShowGallery(false)} />;
+  }
+
   return <EditorPage />;
 }
